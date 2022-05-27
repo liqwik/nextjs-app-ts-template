354086064904e2e1e139973c44fb364fd9f04e2c
Chhay Toch <chhaytoch.dev@gmail.com>
2022-05-27 15:59:46 +0700

    build: add storybook-addon-next

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
@@ -5,6 +7,12 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      name: 'storybook-addon-next',
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
