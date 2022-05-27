679358ec4bb977d4a44361991de262e9edb7ee2c
Chhay Toch <chhaytoch.dev@gmail.com>
2022-05-27 15:58:15 +0700

    feat: implement product card

---
+++ components/cards/product/ProductCard.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import ProductCard, { IProductCard } from './ProductCard';
+import { mockProductCardProps } from './ProductCard.mock';
+
+export default {
+  title: 'cards/ProductCard',
+  component: ProductCard,
+  argTypes: {},
+} as ComponentMeta<typeof ProductCard>;
+
+const Template: ComponentStory<typeof ProductCard> = (args) => (
+  <ProductCard {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockProductCardProps.base,
+} as IProductCard;
