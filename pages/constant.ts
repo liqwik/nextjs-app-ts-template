25cc481cab19a4f196eee64c198219ed755ee98f
Chhay Toch <chhaytoch.dev@gmail.com>
2022-05-30 15:53:46 +0700

    feat: create game grid and use arrow key to move the block

---
+++ pages/constant.ts
@@ -0,0 +1,15 @@
+export const mazesGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export const maxCell = mazesGrid.length;
+export const gridPerRow = mazesGrid.length / 3;
+export const emoji = ['🐶', '🐹', '🐰', '🦊', '🐼', '🦁', '🐔', '🦄', '🐠'];
+export const name = [
+  'Dog',
+  'Mouse',
+  'Rabbit',
+  'Fox',
+  'Panda',
+  'Lion',
+  'Chicken',
+  'Unicorn',
+  'Fish',
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,61 @@
-import ProductCard from '../components/cards/product/ProductCard';
-import { mockProductCardProps } from '../components/cards/product/ProductCard.mocks';
-import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
-import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import { useCallback, useEffect, useState } from 'react';
+import { emoji, gridPerRow, maxCell, mazesGrid, name } from './constant';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+    };
+  }, []);
+
+  const downHandler = useCallback((e: any) => {
+    switch (e.keyCode) {
+      case 37:
+        setSelected((prev) => (prev <= 0 ? prev : prev - 1));
+        break;
+
+      case 38:
+        setSelected((prev) => (prev - gridPerRow <= 0 ? prev : prev - 3));
+        break;
+
+      case 39:
+        setSelected((prev) => (prev >= maxCell - 1 ? prev : prev + 1));
+        break;
+
+      case 40:
+        setSelected((prev) => (prev + gridPerRow > 8 ? prev : prev + 3));
+        break;
+
+      default:
+        break;
+    }
+  }, []);
+
   return (
-    <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <ProductCard {...mockProductCardProps.base} />
+    <section className="h-screen w-screen">
+      <div className="grid grid-cols-3 gap-12 h-full">
+        {mazesGrid &&
+          mazesGrid.map((maze) => (
+            <div
+              key={maze}
+              className={`${
+                maze !== selected ? 'bg-rose-500' : 'bg-rose-300'
+              } rounded-lg p-4 text-white h-48 w-48 place-self-center`}
+            >
+              <p className="text-9xl text-center">{emoji[maze]}</p>
+              {maze === selected && (
+                <p className="text-2xl text-center">{name[maze]}</p>
+              )}
+            </div>
+          ))}
+      </div>
     </section>
   );
 };
 
 export default Home;
-
-Home.getLayout = (page) => {
-  return (
-    <PrimaryLayout>
-      <SidebarLayout />
-      {page}
-    </PrimaryLayout>
-  );
-};
